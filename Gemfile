1af9cfd6c38d00af1a00718199e37e73033b521f
Justin Veirs <justinveirs@gmail.com>
2014-12-14 12:26:20 -0500

    working on js

--- Gemfile
+++ Gemfile
@@ -3,6 +3,5 @@ ruby '2.1.2'
 
 gem 'bootstrap'
 gem 'font-awesome-sass'
-gem 'handlebars_assets'
 gem 'sinatra'
 gem 'thin'
--- public/js/application.js
+++ public/js/application.js
@@ -1,1 +1,40 @@
+var todo = todo || {};
+  data = JSON.parse(localStorgae.getItem("todoData"));
 
+data = data || {};
+
+  (function(todo, data, $) {
+
+      var defaults = {
+          todoTask: "todo-task",
+          todoHeader: "task-header",
+          todoDate: "task-date",
+          todoDescription: "task-Description",
+          taskId: "task-",
+          formId: "todo-form",
+          dataAttribute: "data",
+          deleteDiv: "delete-div",
+        }, codes = {
+          "1" = "#pending",
+          "2" = "#inProgress",
+          "3" = "#completed"
+        };
+
+      todo.init = function (options) {
+
+        options = options || {};
+        options = $.extend({}, defaults, options);
+
+        $.each(data, function (index, params) {
+            generateElement(params);
+        });
+
+        $.each(codes, function (index, value) {
+          $(value).droppable({
+            drop: function (event, ui) {
+              var element = ui.helper,
+              css_id = element.attr("id"),
+              id = css_id.replace(options.taskId, ""),
+              object = data[id];
+
+              removeElement(object);
