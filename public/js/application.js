f152ff9212dc86de3ff802666afab3e18cdfb421
Justin Veirs <justinveirs@gmail.com>
2015-01-06 06:18:21 -0500

    js

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,3 @@
-$('form').submit(function () {
-  if ($('input').val() !== '') {
-    var input_value = $('input').val();
-    $('ul').append('<li>' + input_value + '<a href="" class="holler">x</a></li>');
-  };
-  $('input').val('');
-  return false;
-});
-
-$(document).on('click', 'a', function (e) {
-    e.preventDefault();
-    $(this).parent().remove();
+$(".flash").click(function() {
+  $(".brain").toggleClass('laser');
 });
