775fd3358e71f1f0f49f14a0ce9da70751b743c3
Justin Veirs <justinveirs@gmail.com>
2015-01-11 16:21:35 -0500

    freshen up js

--- public/js/application.js
+++ public/js/application.js
@@ -8,5 +8,10 @@ $(".color").click(function() {
   var red = Math.floor(Math.random() * 255);
   var green = Math.floor(Math.random() * 255);
   var blue = Math.floor(Math.random() * 255);
-  alert(red + "," + green + "," + blue);
+  //generate an RGBA from the three values red, green, and blue
+  var randomRGBA = 'rgba('+red+','+green+','+blue+',1)';
+  //and change the body's background to our random color
+  $("body").css("background", randomRGBA);
+  //display the three values
+  alert(randomRGBA);
 });
