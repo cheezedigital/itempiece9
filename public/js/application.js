38a9efdf89a7f39dd6f896217dc4e218747d6b0e
Justin Veirs <justinveirs@gmail.com>
2015-01-11 16:02:47 -0500

    working this robot

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,12 @@
+// if lasers are off, turn them on, if they're on, turn them off.
 $(".flash").click(function() {
   $(".brain").toggleClass('laser');
+});
+
+// when color button is clicked, pick a number 0-255
+$(".color").click(function() {
+  var red = Math.floor(Math.random() * 255);
+  var green = Math.floor(Math.random() * 255);
+  var blue = Math.floor(Math.random() * 255);
+  alert(red + "," + green + "," + blue);
 });
